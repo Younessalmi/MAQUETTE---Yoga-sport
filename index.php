ed4bbbf237b9f438841c9806c62c8fd039713aaa
Younessalmi <younessalmi193@gmail.com>
2021-08-07 19:52:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ila yoga 2</title>
+    <link  href="./style.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>@import url("https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap");</style>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link rel="stylesheet" href="./w3.css">
+</head>
+
+
+
+
+<body>
+        <header>
+                <nav>
+                    <ul>
+                   
+                    <li>Agenda<i class="fa fa-calendar" style="font-size: x-large;display:block;"></i></li>
+                    
+                    <li>Livre d'or<i class="fa fa-book" style="font-size: x-large;display:block;"></i></li> 
+                     
+                    <li>Album Photo<i class="fa fa-picture-o" style="font-size: x-large;display:block;"></i></li>
+                    
+                    <li>Nous contacter<i class="fa fa-envelope" style="font-size: x-large;display:block;"></i></li>
+
+                    </ul>
+                </nav>
+                <h2 style="background-color:rgba(255,255,255,0.15);font-family:KaPlayfair Displayushan;text-align:center;color:white;font-size:22px;margin:0px;">OFFRE PROMO : Votre premier cours d'essai gratuit !</h2>
+                <p style="font-family:kaushan;color:white;text-align:center;font-size:30px;margin: 98px 0px;" >Bienvenue sur le site d'Ila Yoga</p>
+         </header>
+<div style="background-color:rgba(255,255,255,0.15);">
+
+</div>
+
+
+
+
+<div style="background-color:white;height: 264px;">
+    <h2 style="font-family: 'Playfair Display', serif;font-size:24px;text-align:center;" >Lorem ipsum dolor sit amet</h2>
+
+    <img src="./img/yoga_cours.jpg" style="float:left;margin-right:25px;margin-left:25px;">
+
+    <p style="line-height:1.8;font-family:Playfair Display;font-size:16px;">Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+     ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+     mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae,
+     ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis
+     pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat.
+     Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+</p>
+
+</div>
+
+
+<hr style="border: 0;border-top: 1px solid #eee;margin: 13px 72px;">
+
+
+<div style="background-color:white;height: 264px;">
+<h2 style="font-family:Playfair Display;font-size:24px;text-align:center;" >Lorem ipsum dolor sit amet</h2>
+
+<img src="./img/yoga_posture.png" style="float:right;margin-right:25px;">
+
+<p style="line-height: 1.8;font-family:Playfair Display;font-size: 16px;margin-left:25px;">Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+   ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae 
+   est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
+   Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
+   Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan 
+   porttitor, facilisis luctus, metus
+</p>
+</div>
+
+<div style="background-image:url('./img/background_section.jpg'); background-repeat:no-repeat;background-size: cover;">
+
+    <div style="width: 30%;margin: 1%;padding: 2%;display: inline-block;">
+        <h2 style="font-size:20px;text-align:center;color:white;">Nos cours</h2>
+        <img src="./img/home/1.png" style="width: 150px;margin-left: 140px;">
+        <p style="text-align:center;color:white;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur.
+        </p>  
+    </div>
+
+    <div style="width: 30%;margin: 1%;padding: 2%;display: inline-block;">
+        <h2 style="font-size:20px;text-align:center;color:white;">Nos professeurs</h2>
+        <img src="./img/home/2.png" style="width: 150px;margin-left: 140px;">
+        <p style="text-align:center;color:white;" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur.
+        </p>  
+    </div>
+
+    <div style="width: 30%;margin: 1%;padding: 2%;display: inline-block;">
+        <h2 style="font-size:20px;text-align:center;color:white;">Nos Tarifs</h2>
+        <img src="./img/home/3.png" style="width: 150px;margin-left: 140px;">
+        <p style="text-align:center;color:white;" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur.
+        </p>  
+    </div>
+
+
+</div>
+
+<div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10499.212543918042!2d2.3271878941603745!3d48.861964268604076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e25d0482b3d%3A0x1c0b82c6e1d85100!2s75001%20Paris%2C%20France!5e0!3m2!1sfr!2sma!4v1606147449613!5m2!1sfr!2sma" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+</div>
+
+<footer>
+<div style="background-color:#8B76D2;text-align: center;margin:34px;">
+    <img style="width: 50px;margin: 10px 0 0;" src="./img/footer.png" alt="footer">
+    <p style="text-align:center;font-size:12px;">
+    Cet exercice de <a href="#">3W Academy</a> est mis à disposition <a href="#">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution - 
+    Pas d'Utilisation Commerciale - Pas de Modification - International.</a><br>Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de 
+    <a href="mailto:contact@3wa.fr"> contact@3wa.fr</a>. Les maquettes ont été réalisées par Justine Muller.</p>
+</div>
+</footer>
+</body>
+</html>
